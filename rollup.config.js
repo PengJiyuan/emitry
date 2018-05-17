d422a04516431a432a8a4fda90ac310906761407
PengJiyuan <731401082@qq.com>
2018-05-17 17:19:10 +0800

    add umd and es module

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,16 @@
 import buble from 'rollup-plugin-buble';
 import butternut from 'rollup-plugin-butternut';
 
-const min = process.env.npm_config_min;
+const min = process.env.min;
+
 const config = {
   input: './index.js',
-  output: {
-    format: 'umd',
-    name: 'Emitry',
-    file: './lib/emitry.umd.js'
-  },
   plugins: [
     buble()
   ]
 };
 
 if(min) {
-  config.output.file = './lib/emitry.umd.min.js';
   config.plugins.push(butternut());
 }
 
